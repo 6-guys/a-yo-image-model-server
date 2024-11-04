a30c34e7c2249c134469ab7c4631f15d8f7a1bf5
Jang-myoung-gyoon <mgs556@naver.com>
2024-11-04 11:13:04 +0900

    Update : output size change to (9, 128, 128, 4)

--- model/service.py
+++ model/service.py
@@ -26,12 +26,13 @@ class FrameGenerationService:
     def generate_frames(self,         
             input_array: Annotated[np.ndarray, Shape((128, 128, 4)), DType("float16")]
             = Field(description="A 128x128x4 tensor with float16 dtype")) -> np.ndarray :
-        label = np.zeros(10)
-        label[1] = 1
+        label = np.zeros((9 ,10))
+        for i in range(9) :
+            label[i, 1] = 1
         input_img = np.expand_dims(input_array, axis=0)
-        input_label = np.expand_dims(label, axis=0)
+        input_img = np.repeat(input_img , 9 , axis = 0)
         # Model prediction
-        generated_frames = self.model.predict([input_img , input_label])  # (128, 128, 4)
+        generated_frames = self.model.predict([input_img , label])  # (128, 128, 4)
         print(generated_frames.shape)
         generated_frames = (generated_frames * 255).astype(np.uint8).tolist()  # 리스트로 변환
         return generated_frames
